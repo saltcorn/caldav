5cbb210c3d8a62da3d1063f632a7e908544959eb
Tom Nielsen <tanielsen@gmail.com>
2025-04-10 13:42:59 +0100

    reading cal objects

--- table-provider.js
+++ table-provider.js
@@ -9,12 +9,14 @@ const { getState } = require("@saltcorn/data/db/state");
 const { mkTable } = require("@saltcorn/markup");
 const { pre, code } = require("@saltcorn/markup/tags");
 const { createDAVClient } = require("tsdav");
-const configuration_workflow = (req) =>
+const configuration_workflow = (cfg) => (req) =>
   new Workflow({
     steps: [
       {
         name: "query",
         form: async () => {
+          const client = await getClient(cfg);
+          const cals = await getCals(cfg, client);
           return new Form({
             fields: [
               {
@@ -22,6 +24,7 @@ const configuration_workflow = (req) =>
                 label: "Calendar URL",
                 type: "String",
                 required: true,
+                attributes: { options: cals.map((c) => c.url) },
               },
             ],
           });
@@ -30,13 +33,13 @@ const configuration_workflow = (req) =>
     ],
   });
 
-const runQuery = async (
-  { username, password, auth_method, calendar_url },
-  where,
-  opts
-) => {
+let _allCals;
+
+const getClient = async ({ username, password, auth_method, url }) => {
+  console.log("url", url);
+
   const client = await createDAVClient({
-    serverUrl: calendar_url,
+    serverUrl: url,
     credentials: {
       username,
       password,
@@ -44,8 +47,28 @@ const runQuery = async (
     authMethod: "Basic",
     defaultAccountType: "caldav",
   });
+  return client;
+};
+
+const getCals = async (opts, client0) => {
+  if (_allCals) return _allCals;
+  const client = client0 || (await getClient(opts));
   const calendars = await client.fetchCalendars();
-  console.log(calendars);
+  _allCals = calendars;
+  return calendars;
+};
+
+const runQuery = async (cfg, where, opts) => {
+  const client = await getClient(cfg);
+  const cals = await getCals(cfg, client);
+  const calendar = cals.find((c) => c.url === cfg.calendar_url);
+  console.log("calendar", calendar);
+
+  const objects = await client.fetchCalendarObjects({
+    calendar,
+  });
+
+  console.log(objects);
 
   /* const sqlQ = parser.sqlify(ast, opt);
   console.log({ sqlQ, phValues, opts });
@@ -60,8 +83,8 @@ const runQuery = async (
 
 module.exports = (cfg) => ({
   CalDav: {
-    configuration_workflow,
-    fields: (cfg) => cfg?.columns || [],
+    configuration_workflow: configuration_workflow(cfg),
+    fields: (cfg) => [{ name: "id", type: "Integer" }],
     get_table: (cfgTable) => {
       return {
         getRows: async (where, opts) => {
